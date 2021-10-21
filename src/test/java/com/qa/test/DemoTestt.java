9a7cebe3f1666eb5a9e4f0ce524f023f177822f6
Ragavi123 <svrajan124@gmail.com>
2021-10-21 16:59:29 +0530

    Demo

---
+++ src/test/java/com/qa/test/DemoTestt.java
@@ -0,0 +1,44 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class DemoTestt {
+	
+	@Test
+	public void browserlaunch() {
+		System.out.println("Launch Browser");
+}
+	@Test
+	private void getUrl() {
+		System.out.println("Print the URL");
+	}
+	
+	@Test
+	private void screenMax() {
+		System.out.println("Maximize the screen");
+	}
+	
+	@Test
+	private void closAll() {
+		System.out.println("Close everything");
+
+	}
+
+	}
+	
+	
+	
+		
+	
+		
+		
+	
+	
+	
+		
+		
+
+	
+	
+
+
